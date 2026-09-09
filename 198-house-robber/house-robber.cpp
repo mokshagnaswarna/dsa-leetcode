e7a29208e0ee98ddc4da6d75b6e2764642a81bea
mokshagnaswarna <mokshagnaswarna58@gmail.com>
2026-09-09 10:32:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.59%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int func(int index,vector<int>& nums,int n,vector<int>& dp){
-        //base case
-        
-        if(index<0){
-            return 0;
-        }
-        if(dp[index]!=-1){
-            return dp[index];
-        }
-        int left=nums[index]+func(index-2,nums,n,dp);
-        int right=func(index-1,nums,n,dp);
-        return dp[index]=max(left,right);
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        return func(n-1,nums,n,dp);
+        vector<int>dp(n,-1);
+        if(n == 1)
+            return nums[0];
+
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int take=dp[i-2]+nums[i];
+            int skip=dp[i-1];
+            dp[i]=max(take,skip);
+        }
+        return dp[n-1];
     }
 };
